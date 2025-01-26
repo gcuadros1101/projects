0f0e686c5bfe6024a52c0f48aba9d02940295bba
Giancarlo Cuadros Moreno <giancarlo.uw@gmail.com>
2025-01-25 19:39:46 -0800

    Adding the source files

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+declare module 'lodash-move' {
+    const fn: (list: number[], b: number, c: number) => number[]
+    export default fn
+  }
+  declare module 'lodash.clamp' {
+    const clamp: (number: number, lower: number, upper: number) => number;
+    export default clamp;
+  }
