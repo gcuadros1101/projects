b05132491793b153584403302e5d54cfdf8e902c
Michelle Cuadros <miccuadr@amazon.com>
2025-02-11 23:29:54 -0800

    Add spinner on RSVP

--- src/components/RSVPModal/RSVPModal.tsx
+++ src/components/RSVPModal/RSVPModal.tsx
@@ -1,30 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RSVPModal.css";
+import "../../App.css";
+
 
 type RSVPModalProps = {
     isOpen: boolean;
     onClose: () => void;
-    onRSVP: (rsvp: boolean) => void;
-};
+    onRSVP: (rsvp: boolean) => Promise<void>; // Assume onRSVP is async and returns a promise
+  };
 
 
 const RSVPModal: React.FC<RSVPModalProps> = ({ isOpen, onClose, onRSVP }) => {
+  const [loading, setLoading] = useState(false);
+
   if (!isOpen) return null;
+
+  const handleRSVP = async (rsvp: boolean) => {
+    setLoading(true);
+    try {
+      await onRSVP(rsvp);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         <h2 className="modal-title">RSVP to the Party!</h2>
-        {/* <p className="modal-text">Can we count on you to join the fun?</p> */}
         <div className="modal-buttons">
-          <button className="modal-button yes" onClick={() => onRSVP(true)}>
-            Yes, I'll be there!
+          <button className="modal-button yes" onClick={() => handleRSVP(true)} disabled={loading}>
+        Yes, I'll be there!
           </button>
-          <button className="modal-button no" onClick={() => onRSVP(false)}>
-            Won't make it
+          <button className="modal-button no" onClick={() => handleRSVP(false)} disabled={loading}>
+          Won't make it
           </button>
         </div>
-        <button className="modal-close-button" onClick={onClose}>
+        {loading && (
+          <div className="spinner">
+            <span></span> {/* This is where the spinner animation occurs */}
+          </div>
+        )}
+        <button className="modal-close-button" onClick={onClose} disabled={loading}>
           Close
         </button>
       </div>
